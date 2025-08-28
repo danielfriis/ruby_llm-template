2184cc947ad9343b74dfddc9fb09fdc24f16add7
Daniel Friis <d@friis.me>
2025-08-28 19:51:15 +0200

    first commit

---
+++ lib/ruby_llm/template.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "template/version"
+
+module RubyLlm
+  module Template
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
